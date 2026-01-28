e37beb28c0b569959abc8c5414ab6bb205917b95
juan vivenes <vivenesjuan332@gmail.com>
2026-01-28 01:28:04 +0000

    fix: Configure Metro to resolve '@/ aliases'

--- mobile/ConectameApp/metro.config.js
+++ mobile/ConectameApp/metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path'); // Import path module
 
 /**
  * Metro configuration
@@ -6,6 +7,14 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Map '@' to the 'src' directory
+    },
+    // Ensure all packages are resolved from the local node_modules
+    nodeModulesPaths: [path.resolve(__dirname, './node_modules')],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
